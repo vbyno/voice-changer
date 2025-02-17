14831eb3fefd7861d30105e6713eef2358d79843
Volodia <v.byno@steel.mil.ua>
2025-02-17 13:25:17 +0200

    Add publicPath to webpack generation

--- client/demo/webpack.prod.js
+++ client/demo/webpack.prod.js
@@ -1,6 +1,12 @@
+const path = require("path");
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     mode: 'production',
+    output: {
+        publicPath: '/voice_changer/',
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist"),
+    },
 })
